481b0f440da3d3e35e73229d1e906e5f99c5b037
Hawk Studio <magk34229@gmail.com>
2026-08-30 13:46:09 +0200

    Update backrounds.js

--- backrounds.js
+++ backrounds.js
@@ -1,9 +1,9 @@
 const backrounds =[
-    "backrounds/1.png",
-    "backrounds/2.png",
-    "backrounds/3.png",
-    "backrounds/4.png",
-    "backrounds/5.png",
+    "../backrounds/1.png",
+    "../backrounds/2.png",
+    "../backrounds/3.png",
+    "../backrounds/4.png",
+    "../backrounds/5.png",
 ];
 
 const interval = 2000;
